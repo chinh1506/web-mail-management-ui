76b433fc0da9d70bd9ffbae1b7f6fcbefdfa35ab
AnhDuc57 <anhduc1257@gmail.com>
2022-06-29 22:39:11 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
+import Login from './layouts/Login';
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
                     <Route path="/" element={<DefaultLayout />}></Route>
+                </Routes>
+                <Routes>
+                    <Route path="/login" element={<Login />}></Route>
                 </Routes>
             </div>
         </Router>
--- src/layouts/Login/index.js
+++ src/layouts/Login/index.js
@@ -3,7 +3,31 @@ import styles from './Login.module.scss';
 
 const cx = classNames.bind(styles);
 function Login() {
-    return <div className={cx('wrapper')}></div>;
+    return <div className={cx('wrapper')}>
+        <div className={cx('container')}>
+            <div className={cx('wrap-logo')}>
+                        <img
+                            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+                            alt=""
+                            className={cx('logo')}
+                        />
+            </div>
+            <div className={cx('header')}>ĐĂNG NHẬP</div>
+            <div className={cx('content')}>
+                <div className={cx('login')}>
+                    <h3>Tài khoản</h3>
+                    <input type="text" size="30"/>
+                </div>
+                <div className={cx('password')}>
+                    <h3>Mật khẩu</h3>
+                    <input type="text" size="30"/>
+                </div>
+            </div>
+            <div className={cx('footer')}>
+                <button>Đăng nhập</button>
+            </div>
+        </div>
+    </div>;
 }
 
 export default Login;
